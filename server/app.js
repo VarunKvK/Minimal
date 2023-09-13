4d64df83b86f27141b013183707be764119370df
VarunKvK <varunkrishnankv2002@gmail.com>
2023-09-13 19:36:39 +0530

    Minimal gitIgnores

--- server/app.js
+++ server/app.js
@@ -1,7 +1,22 @@
-require("dot-env").config()
+require("dotenv").config()
 const express=require("express");
 const bodyParser=require("body-parser")
 const bcrypt=require("bcrypt")
 const cookieParser=require("cookie-parser")
 const cors=require("cors")
 const mongoose=require("mongoose")
+
+const app=express()
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({origin:true,credentials:true}))
+
+
+
+
+
+app.listen(8000,()=>{
+    console.log("Server Running on 8000")
+})
